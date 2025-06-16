285d6d5b7f51ca9149a7aeb92ed8ee416a74594e
s1thu <s1thu.w1n.5601@gmail.com>
2025-06-16 22:09:54 +0630

    Add interface and struct examples for Simpler and Shaper

---
+++ interface/challenge.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Simpler interface {  // interface implementing functions called on Simple struct
+	Get() int
+	Set(int)
+}
+
+type Simple struct {
+	i int
+}
+
+func (p *Simple) Get() int {
+	return p.i
+}
+
+func (p *Simple) Set(u int) {
+	p.i = u
+}
+
+func fI(it Simpler) int {     // function calling both methods through interface
+	it.Set(5)
+	return it.Get()
+}
+
+func main() {
+	var s Simple	
+	fmt.Println(fI(&s))  // &s is required because Get() is defined with a receiver type pointer
+}
