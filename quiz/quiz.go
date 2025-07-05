fd51755aa599ab470858fa1a00dd34e869980bad
s1thu <s1thu.w1n.5601@gmail.com>
2025-07-05 15:35:42 +0630

    Refactor quiz.go by removing unused time zone constants and simplifying main function logic

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,35 +1,39 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type TZ int
-
-const (
-	HOUR TZ = 60 * 60
-	UTC  TZ = 0 * HOUR
-	EST  TZ = -5 * HOUR
-	CST  TZ = -6 * HOUR
-)
-
-var timeZones = map[TZ]string{
-	UTC: "Universal Greenwich time",
-	EST: "Eastern Standard time",
-	CST: "Central Standard time",
-}
-
-func (tz TZ) String() string {
-	for name, zone := range timeZones {
-		if tz == name {
-			return zone
-		}
-	}
-	return ""
-}
+type obj interface{}
 
 func main() {
-	fmt.Println(EST) // Print knows about method String() of type TZ
-	fmt.Println(0 * HOUR)
-	fmt.Println(-6 * HOUR)
+	mf := func(i obj) obj {
+		switch i.(type) {
+		case int:
+			return i.(int) * 2
+		case string:
+			return i.(string) + i.(string)
+		}
+		return i
+	}
+
+	isl := []obj{0, 1, 2, 3, 4, 5}
+	res1 := mapFunc(mf, isl)
+	for _, v := range res1 {
+		fmt.Println(v)
+	}
+	fmt.Println()
+
+	ssl := []obj{"0", "1", "2", "3", "4", "5"}
+	res2 := mapFunc(mf, ssl)
+	for _, v := range res2 {
+		fmt.Println(v)
+	}
+}
+
+func mapFunc(mf func(obj) obj, list []obj) []obj {
+	result := make([]obj, len(list))
+
+	for ix, v := range list {
+		result[ix] = mf(v)
+	}
+	return result
 }
